b17cc336a4c831977d57f03a0e7b93f56ad643b0
Krassowsky <krassowsky@MacBook-Pro-Krassowsky.local>
2021-03-25 17:19:13 +0800

    Color Card Key Prop added

--- src/components/ColorCard.js
+++ src/components/ColorCard.js
@@ -2,15 +2,17 @@ import Rating from "./Rating"
 
 const ColorCard = ({ color }) => {
   return (
-    <div className="card">
-      <Rating rating={color.rating} />
-      <div
-        className="card-color"
-        style={{ backgroundColor: color.color }}
-      ></div>
-      <div className="card-body text-center">
-        <h5 className="card-title">{color.title}</h5>
-        <button className="btn btn-primary">Delete</button>
+    <div className="col-sm-4">
+      <div className="card">
+        <Rating rating={color.rating} />
+        <div
+          className="card-color"
+          style={{ backgroundColor: color.color }}
+        ></div>
+        <div className="card-body text-center">
+          <h5 className="card-title">{color.title}</h5>
+          <button className="btn btn-primary">Delete</button>
+        </div>
       </div>
     </div>
   )
--- src/components/ColorCards.js
+++ src/components/ColorCards.js
@@ -1,14 +1,16 @@
 import ColorCard from "./ColorCard"
 
-const ColorCards = ({ colors }) => {
+const ColorCards = ({ colors=[] }) => {
   console.log(colors)
   return (
     <div className="row mt-3">
-      {colors.map((color) => (
-        <div className="col-sm-4">
-          <ColorCard color={color} />
-        </div>
-      ))}
+      {(colors.length) ?
+            colors.map((color) => (
+              <ColorCard key={color.id} color={color} />
+            ))
+      :
+      <p>No colors listed. Add a color.</p>
+      }
     </div>
   )
 }
