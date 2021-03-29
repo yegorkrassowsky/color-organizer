3e527ad24b7da97863c931cf0602c4d31de8c8f4
Krassowsky <krassowsky@MacBook-Pro-Krassowsky.local>
2021-03-29 20:23:51 +0800

    Alert on Rating Change added

--- src/components/ColorCard.js
+++ src/components/ColorCard.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, {useRef, useEffect} from "react"
 import Rating from "./Rating"
 
 const ColorCard = ( { color, onRemove=f=>f, onRate=f=>f } ) => {
+  const cardRef = useRef(null)
+  const prevRatingRef = useRef(false);
+  useEffect(()=>{
+    const prevRating = prevRatingRef.current;
+    if(prevRating) {
+      cardRef.current.style.borderColor = "red"
+      alert(`${color.title}: rating ${prevRating} -> ${color.rating}`)
+      cardRef.current.style.removeProperty("border-color")
+    }
+    prevRatingRef.current = color.rating
+  })
   return (
     <div className="col-sm-4">
-      <div className="card">
+      <div className="card" ref={cardRef}>
         <Rating rating={color.rating} onRate={onRate} />
         <div
           className="card-color"
